62170abeb59f30fa435a3d5b7f1b1de5d622ab57
Andrei Picus <andrei.picus@tink.se>
2021-01-26 11:31:58 +0100

    chore: Remove AppController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./controllers/app.controller";
-import { AppService } from "./services/app.service";
 import { CatsModule } from "./Cats.module";
 
 @Module({
   imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
